77b20ed9f3edd94c90ef3b6e361f91233bbeb8e0
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-11-20 09:21:31 +0700

    get element & children element

--- latian2/script.js
+++ latian2/script.js
@@ -2,3 +2,10 @@ const gambar = document.getElementById("gambar");
 
 gambar.setAttribute("width", "300");
 gambar.setAttribute("height", "215");
+
+
+const buttons = document.querySelectorAll(".button");
+const playButton = buttons[3]; // Get div button play
+
+const playButtonElement = playButton.children[0]; // Get children button play
+console.log(playButtonElement);
